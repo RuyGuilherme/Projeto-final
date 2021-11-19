461ffd0b26915404fed7cd7d68857c8604f098f6
RuyGuilherme <89050460+RuyGuilherme@users.noreply.github.com>
2021-11-18 22:00:26 -0300

    atualização 2

--- models/catalogo.js
+++ models/catalogo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const database = require("./../database");
 const Sequelize = require("sequelize");
 
@@ -33,40 +32,4 @@ const Artigo = database.define("artigo", {
 });
 
 
-=======
-const database = require("./../database");
-const Sequelize = require("sequelize");
-
-const Artigo = database.define("artigo", {
-     
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  nome: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  descricao: Sequelize.STRING,
-  titulo: Sequelize.STRING,
-  imagem: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-},
-{
-  freezeTableName: true,
-  timestamps: false, 
-  createdAt: false,
-  updatedAt: false,
-});
-
-
->>>>>>> 3708d397a04091178b239bd68cf25298510f0662
 module.exports = Artigo;
